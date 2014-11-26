5417d8621d95a26d95840996e678a4b3cac75717
Guillaume Coste <guillaume.coste@gmail.com>
2014-11-26 21:43:05 +0100

    changment de label

--- src/components/Home/AlertBar.js
+++ src/components/Home/AlertBar.js
@@ -20,7 +20,7 @@ module.exports = React.createClass({
             <div className="row alertBar">
                 <AlertIcon text="Plus de victoires" data={this.props.data.victory} icon="fa fa-thumbs-up" type="bg-success-light"></AlertIcon>
                 <AlertIcon text="Plus de défaites" data={this.props.data.defeat} icon="fa fa-thumbs-down" type="bg-primary-light"></AlertIcon>
-                <AlertIcon text="Nombre de parties jouées" data={this.props.data.games} icon="fa fa-users" type="blue-grey"></AlertIcon>
+                <AlertIcon text="Parties jouées" data={this.props.data.games} icon="fa fa-users" type="blue-grey"></AlertIcon>
                 <AlertIcon text="Dernier au classement" data={this.props.data.last} icon="fa fa-trophy" type="bg-warning-light"></AlertIcon>
                 <AlertIcon text="La passoire" data={this.props.data.worst} icon="fa fa-life-ring" type="bg-green-light"></AlertIcon>
                 <AlertIcon text="Suceurs de cannes" data={this.props.data.fanny} icon="fa fa-frown-o" type="bg-pink-light"></AlertIcon>
