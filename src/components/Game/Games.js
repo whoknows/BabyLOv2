32da12ab7f59165f28928c5037f8f301bd042927
gcoste <guillaume.coste@livingobjects.fr>
2014-11-27 15:25:25 +0100

    game page

--- src/components/Game/Games.js
+++ src/components/Game/Games.js
@@ -1,14 +1,25 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var GameTable = require('components/Table/GameTable.js');
+var GameStore = require('stores/GameStore.js');
 
 module.exports = React.createClass({
-    componentDidMount: function() {
-        //
+    mixins: [
+        Reflux.listenTo(GameStore,"onGameChange")
+    ],
+    onGameChange: function() {
+        this.setState({
+            games: GameStore.getGames()
+        });
     },
     render: function () {
         return (
-            <div id="userChart"></div>
+            <div className="row">
+                <ColPanel col="12" icon="futbol-o" title="Dernières parties">
+                    <GameTable data={this.state.games}></GameTable>
+                </ColPanel>
+            </div>
         );
     }
 });
