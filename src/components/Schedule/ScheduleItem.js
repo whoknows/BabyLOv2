05e58c2f6e61290603804b5a821e866eaeca5aa3
gcoste <guillaume.coste@livingobjects.fr>
2014-11-27 14:49:44 +0100

    shdule item generic

---
+++ src/components/Schedule/ScheduleItem.js
@@ -0,0 +1,38 @@
+/** @jsx React.DOM */
+
+var UserImage = require('components/User/UserImage.js');
+var {Button} = require('react-bootstrap');
+var CurrentUserStore = require('stores/CurrentUserStore.js');
+var ScheduleAction = require('actions/ScheduleAction.js');
+
+module.exports = React.createClass({
+    userScheduled: false,
+    getUserList: function() {
+        var currentUser = CurrentUserStore.getCurrentUser();
+
+        return this.props.users.map(function(user){
+            if(user.id == currentUser.id){
+                this.userScheduled = true;
+                return <UserImage schedule={this.props.creneau} user={user}></UserImage>;
+            }
+
+            return <UserImage user={user}></UserImage>;
+        });
+    },
+    getButton: function () {
+        if(!this.userScheduled){
+            return <Button data-schedule={creneau} onClick={this.clickHandler} bsStyle="success">GO</Button>;
+        }
+    },
+    clickHandler: function (e) {
+        ScheduleAction.participate(e.target.dataset.schedule);
+    },
+    render: function () {
+        return (
+            <div>
+                {this.getUserList()}
+                {this.getButton()}
+            </div>
+        );
+    }
+});
