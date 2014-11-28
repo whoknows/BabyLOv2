d8576ca341b60c319c7c2516019b851eaad2f671
gcoste <guillaume.coste@livingobjects.fr>
2014-11-28 11:51:36 +0100

    form add game

--- src/components/AddGame/AddGame.js
+++ src/components/AddGame/AddGame.js
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var {Input,Button} = require('react-bootstrap');
 
 module.exports = React.createClass({
     mixins: [
@@ -11,11 +11,41 @@ module.exports = React.createClass({
             <div className="content-wrapper">
                 <h3>Ajouter une partie</h3>
                 <div className="row-fluid">
-                    <div className="col-md-6">
-                        <h3>Equipe 1</h3>
+                    <div className="col-md-12">
+                        <h4>Date</h4>
+                        <div className="row">
+                            <div className="col-md-3">
+                                <Input ref="date" type="text" placeholder="Date" value="" />
+                            </div>
+                        </div>
                     </div>
                     <div className="col-md-6">
-                        <h3>Equipe 2</h3>
+                        <h4>Equipe 1</h4>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <Input ref="p1t1" type="text" placeholder="Joueur 1" value="" />
+                            </div>
+                            <div className="col-md-12">
+                                <Input ref="p2t1" type="text" placeholder="Joueur 2" value="" />
+                            </div>
+                            <div className="col-md-12">
+                                <Input ref="scoret1" type="text" placeholder="Score" value="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <h4>Equipe 2</h4>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <Input ref="p1t2" type="text" placeholder="Joueur 1" value="" />
+                            </div>
+                            <div className="col-md-12">
+                                <Input ref="p2t2" type="text" placeholder="Joueur 2" value="" />
+                            </div>
+                            <div className="col-md-12">
+                                <Input ref="scoret2" type="text" placeholder="Score" value="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
