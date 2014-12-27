fd2969675216d79950926ce14804cfce59404aa2
Guillaume Coste <guillaume.coste@gmail.com>
2014-12-27 10:35:04 +0100

    recherche partie avancée

--- src/components/Game/Games.js
+++ src/components/Game/Games.js
@@ -6,6 +6,7 @@ var GameActions = require('actions/GameAction.js');
 var ColPanel = require('components/Home/ColPanel.js');
 var {Button, Input} = require('react-bootstrap');
 var {State} = require('react-router');
+var Select = require('components/Select/Select.js');
 
 require('./Game.css');
 
@@ -36,17 +37,51 @@ module.exports = React.createClass({
 
         return {
             games: gameStoreData,
-            date: dateString
+            date: dateString,
+            formVisible: false
         };
     },
     handleSubmit: function(e){
         e.preventDefault();
+        var data = {date: this.state.date};
+
+        if (this.state.formVisible) {
+            data.users = [
+                this.refs.p1.getValue(),
+                this.refs.p2.getValue()
+            ];
+            data.mode = this.refs.mode.getValue();
+        }
+
         GameActions.loadGames(this.state.date);
     },
     handleChange: function(ref){
         var s = {};
         s[ref] = this.refs[ref].getValue().trim();
         this.setState(s);
+    },
+    handleDisplay: function(e){
+        e.preventDefault();
+
+        this.setState({formVisible: !this.state.formVisible});
+    },
+    getForm: function(){
+        return (
+            <div className="row">
+                <div className="col-md-5">
+                    <Select ref="p1" placeholder="Joueur 1" />
+                </div>
+                <div className="col-md-2 select-container">
+                    <Input type="select" ref="mode" className="middle-select">
+                        <option value="avec">Avec</option>
+                        <option value="contre">Contre</option>
+                    </Input>
+                </div>
+                <div className="col-md-5">
+                    <Select ref="p2" placeholder="Joueur 2" />
+                </div>
+            </div>
+        );
     },
     render: function () {
         return <div className="content-wrapper">
@@ -57,6 +92,8 @@ module.exports = React.createClass({
                                 <form className="form-inline gameform" ref="gameform" onSubmit={this.handleSubmit}>
                                     <Input ref="date" type="text" placeholder="Date" onChange={this.handleChange.bind(this, 'date')} value={this.state.date} />
                                     <Input type="submit" bsStyle="success" value="Valider" />
+                                    <a href="#" onClick={this.handleDisplay}>Recherche avancée</a>
+                                    {this.state.formVisible ? this.getForm() : null}
                                 </form>
                             </div>
                             <GameTable advancedDisplay={true} data={this.state.games} />
