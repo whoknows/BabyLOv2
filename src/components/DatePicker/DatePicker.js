0b7494e10800a326598b481b30cbe5aa3e4d741e
Guillaume Coste <guillaume.coste@gmail.com>
2015-01-01 14:40:18 +0100

    new datepicker

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -1,71 +1,35 @@
-var DayPicker = require('react-day-picker');
+var DatePicker = require('react-date-picker');
 var moment = require('moment');
 var {Input} = require('react-bootstrap');
 
+require('react-date-picker/index.css');
 require('./DatePicker.css');
-
-function dateToValue(d) {
-    return d.format("YYYY-MM-DD");
-}
-
-function valueToDate(s) {
-    var date = moment(s, "YYYY-MM-DD", true);
-    return date.isValid() ? date : null;
-}
-
-function isSameDay(a, b) {
-    return a.startOf('day').isSame(b.startOf('day'));
-}
 
 module.exports = React.createClass({
     getDefaultProps: function(){
         return {inputClass: ''};
     },
     getInitialState: function(){
-        return {value: dateToValue(moment()), visible: false};
-    },
-    onDayClick: function(day, modifiers, e) {
-        this.setState({ value: dateToValue(day) });
+        return {value: moment().format("YYYY-MM-DD"), visible: false};
     },
     handleInputChange: function(e) {
-        this.setState({ value: e.target.value });
+        this.setState({value: e.target.value});
     },
     toggleDatepicker: function() {
         this.setState({visible: !this.state.visible});
     },
-    handleMonthChange: function(e) {
-        console.log('month');
+    onChange: function(moment, dateString) {
+        this.setState({value: dateString, visible: false});
     },
-    /*handleNextTouchTap(e) {
-        this.setState({ month: this.state.month.clone().add(1, 'month') }, () => {
-            this.props.onNextMonthTouchTap
-            && this.props.onNextMonthTouchTap(this.state.month);
-        });
-    },
-    handlePrevTouchTap(e) {
-        this.setState({ month: this.state.month.clone().subtract(1, 'month') }, () => {
-            this.props.onPrevMonthTouchTap
-            && this.props.onPrevMonthTouchTap(this.state.month);
-        });
-    },*/
     getValue: function(){
         return this.state.value;
     },
     render: function() {
-        var modifiers = {
-            today: function (day) {
-                return isSameDay(moment(), day);
-            },
-            selected: function (day) {
-                return isSameDay(valueToDate(this.state.value), day);
-            }.bind(this)
-        };
-
         return (
             <div className="datepicker-wrapper">
                 <Input type="text" ref="datepicker" className={this.props.inputClass} value={this.state.value} onClick={this.toggleDatepicker} onChange={this.handleInputChange} />
                 {this.state.visible ?
-                    <DayPicker modifiers={modifiers} enableOutsideDays={true} onDayClick={this.onDayClick} onNextMonthTouchTap={this.handleMonthChange} onPrevMonthTouchTap={this.handleMonthChange} />
+                    <DatePicker hideFooter={true} date={this.state.value} onChange={this.onChange} />
                 : null}
             </div>
         );
