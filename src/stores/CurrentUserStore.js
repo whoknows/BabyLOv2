f394d5c641c26cb4a8e35c80d2e6bc200bb087a2
Guillaume Coste <guillaume.coste@gmail.com>
2015-01-03 18:51:42 +0100

    prevent undefined

--- src/stores/CurrentUserStore.js
+++ src/stores/CurrentUserStore.js
@@ -57,9 +57,9 @@ module.exports = Reflux.createStore({
         return this.currentUser;
     },
     isAdmin: function(){
-        return (typeof this.currentUser.roles !== 'undefined' && this.currentUser.roles.indexOf('ROLE_ADMIN') !== -1);
+        return (this.currentUser && this.currentUser.roles && this.currentUser.roles.indexOf('ROLE_ADMIN') !== -1);
     },
     isSuperAdmin: function(){
-        return (typeof this.currentUser.roles !== 'undefined' && this.currentUser.roles.indexOf('ROLE_SUPER_ADMIN') !== -1);
+        return (this.currentUser && this.currentUser.roles && this.currentUser.roles.indexOf('ROLE_SUPER_ADMIN') !== -1);
     }
 });
